05d634289b00d3e3452c8760d2f8a2aac72d2922
Ron Mares <ron.mares@mktec.com>
2017-05-08 20:06:13 -0400

    finish weather app

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,18 +10,39 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
-  $scope.country = "US";
-  $scope.city = "Sanford";
+  $scope.far = true;
   $scope.data = {};
+  $scope.temp = $scope.main
 
   $http({
      method: "GET",
      url: 'http://api.openweathermap.org/data/2.5/weather?q=Sanford,nc&appid=192ec8501562aee106a44018e236f502',
   }).then(function(res) {
-    $scope.data = res.data;
-    console.log($scope.data);
+      $scope.data = res.data;
+      $scope.temp = kelToFar($scope.data.main.temp);
+      $scope.descr = $scope.data.weather[0].main;
+      $scope.icon = $scope.data.weather[0].icon;
+      $scope.country = $scope.data.sys.country;
+      $scope.name = $scope.data.name;
+      console.log($scope.data);
   }, function errorCallback(res) {
-    //$scope.data = res.status;
+       $scope.data = res.status;
   });
 
+  function kelToFar(temp) {
+      return 9/5 * (temp - 273) + 32;
+  }
+
+  function farToCel(temp) {
+    return (temp - 32) * .5556;
+  }
+
+  function celToFar(temp) {
+    return (temp * 1.8) + 32;
+  }
+
+  $scope.swapTemp = function() {
+      $scope.far = !$scope.far;
+      return $scope.temp = $scope.far ? celToFar($scope.temp) : farToCel($scope.temp);
+  }
 }]);
